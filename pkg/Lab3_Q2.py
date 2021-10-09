812068c72cdffafe0e9afce27031044a4a7bb84c
Vitaliy Baseckas <L00169827@student.lyit.ie>
2021-10-09 16:58:18 +0100

    Added Lab3_Q1 and Lab3_Q2

---
+++ pkg/Lab3_Q2.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------------------------
+# Name:        Lab3_Q2.py
+# Project:     Python_Programming_Labs
+#
+# Author:      Vitaliy Baseckas
+#
+# Created:     09.10.2021
+# Copyright:   (c) Vitaliy Baseckas 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
